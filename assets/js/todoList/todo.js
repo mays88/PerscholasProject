cce62ceb41f8d5201469d6f49187517eefb88de1
Mays88 <antmays.ceo@gmail.com>
2024-05-07 14:59:55 -0400

    Updated responsiveness

--- assets/js/todoList/todo.js
+++ assets/js/todoList/todo.js
@@ -18,24 +18,40 @@ export async function getTask() {
 }
 
 function populateTask(postId) {
-    let allTask = document.querySelector(".navbar-nav");
+    let allTask = document.getElementById("todo-list");
     let allPost = postId.todos;
 
     // console.log(allTask);
     for (let post in allPost) {
         let reqPost = allPost[post];
-        // let commentEmail = document.createElement("h4");
+        let taskLi = document.createElement("li");
+        let taskContainer = document.createElement("div");
+        let taskCat = document.createElement("p");
+        let taskTitle = document.createElement("p");
+        let taskSpacer = document.createElement("span");
+        let taskDate = document.createElement("p");
+        // let taskDate = new Date();
 
         let todoTitle = reqPost.todo;
-        // let todoTitle = reqPost.todo;
-        // let todoTitle = reqPost.todo;
-        // let commentBody = document.createElement("p");
-        // let userEmail = comment.email;
-        // let commentText = comment.body;
-        // commentEmail.textContent = userEmail;
-        // commentBody.textContent = commentText;
-        // allComment.appendChild(commentEmail);
-        // allComment.appendChild(commentBody);
+
+        allTask.appendChild(taskLi);
+        taskLi.appendChild(taskDate);
+        taskLi.appendChild(taskSpacer);
+        taskLi.appendChild(taskContainer);
+        taskContainer.appendChild(taskCat);
+        taskContainer.appendChild(taskTitle);
+
+        taskLi.classList.add("todo-task");
+        taskContainer.classList.add("todo-task-container");
+        taskDate.classList.add("todo-task-date");
+        taskSpacer.classList.add("todo-task-span");
+        taskCat.classList.add("todo-task-category");
+        taskTitle.classList.add("todo-task-title");
+
+        taskDate.textContent = "6:00 - 7:00";
+        taskCat.textContent = "Finance";
+        taskTitle.textContent = todoTitle;
+
         console.log(allPost[post]);
     }
 
