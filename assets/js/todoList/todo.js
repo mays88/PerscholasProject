12fb48facdba9a5e5cf425dfffe24a41aafa253a
Mays88 <antmays.ceo@gmail.com>
2024-05-07 08:56:09 -0400

    Added getTask to get list of task for todo list

--- assets/js/todoList/todo.js
+++ assets/js/todoList/todo.js
@@ -1,3 +1,43 @@
-// export function highlightElement() {
-//     console.log("Test");
-// }
+export async function getTask() {
+    try {
+        const response = await axios.get(`https://dummyjson.com/todos`);
+        // const postId = postInput.value;
+        // const postTitle = document.querySelector("#postTitle");
+        // const postBody = document.querySelector("#postBody");
+
+        // postTitle.textContent = response.data.title;
+        // postBody.textContent = response.data.body;
+
+        populateTask(response.data);
+        // const result = response.data;
+        // console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function populateTask(postId) {
+    let allTask = document.querySelector(".navbar-nav");
+    let allPost = postId.todos;
+
+    // console.log(allTask);
+    for (let post in allPost) {
+        let reqPost = allPost[post];
+        // let commentEmail = document.createElement("h4");
+
+        let todoTitle = reqPost.todo;
+        // let todoTitle = reqPost.todo;
+        // let todoTitle = reqPost.todo;
+        // let commentBody = document.createElement("p");
+        // let userEmail = comment.email;
+        // let commentText = comment.body;
+        // commentEmail.textContent = userEmail;
+        // commentBody.textContent = commentText;
+        // allComment.appendChild(commentEmail);
+        // allComment.appendChild(commentBody);
+        console.log(allPost[post]);
+    }
+
+    // console.log(allPost[0].id);
+}
